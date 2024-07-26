595ab86716947bd057dcd1fda23b78e3ab6ed216
super-time1 <168475235+super-time1@users.noreply.github.com>
2024-07-26 05:28:07 -0400

    *update

--- index.js
+++ index.js
@@ -2,103 +2,48 @@ const express = require("express");
 const mongoose = require("mongoose");
 require('dotenv').config();
 
+
+// Connection
 main().catch(err => console.log(err));
 
 async function main() {
   await mongoose.connect(process.env.ATLAS_URI)
-}
+};
+const PORT = 2000;
+// Connection^^ 
+
 
 const bodyParser = require("body-parser");
 
-const PORT = 2000; 
-const app = express(); 
+const app = express();
 
+// Middleware 
 app.use(express.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
-const user = require("./model/user");
-const person = require("./model/person");
-const comment = require("./model/comment");
+// Routes
+const userModel = require("./model/user");
+const personModel = require("./model/person");
+const commentModel = require("./model/comment");
 
-const userRouter = require("./routes/user");
-const personRouter = require("./routes/person");
-const commentRouter = require("./routes/comment");
+const user = require("./routes/user");
+app.use("/user", user);
 
-
-
-app.use("", userRouter);
-app.use("", personRouter);
-app.use("", commentRouter);
-
-app.set("view engine", "ejs");
-app.use("/public", express.static("/public")); 
-app.use("/public/media", express.static("/public")); 
-
-
+// Welcome
 app.get("/", (req,res) => {
     res.send("Welcome to (In a Vaccum)");
 });
+// Welcome^^
 
 
-app.get("/new", async (req, res) => {
-    // try{
-    //    const users = await user.find({});
-    //    res.status(200).json(users); 
-    // } catch (error) {
-    //     res.status(500).json({message: error.message})
-    // }
-});
-
-app.post("/new", async (req, res) => {
-    // try {
-    //     const brandNew = await user.create(req.body);
-    //     res.status(200).json(brandNew)
-    // } catch (error) {
-    //     res.status(500).json({message: error.message})
-    // }
-
-});
-
-app.put("/new/:id", async (req, res) => {
-    // try {
-    //     const {id} = req.params
-    //     const update = await user.findByIdAndUpdate(id, req.body);
-        
-    //     if (!user) {
-    //         return res.status(404).json({message: "User does not exist in system"});
-    //     }
-    //     const updateUser = await user.findById(id);
-    //     res.status(200).json(update);
-       
-    //     console.log("System Updated!")
-
-    // } catch (error) {
-    //     res.status(500).json({message:error.message});
-    // }
-});
-
-app.delete("/new/:id", async (req, res) => {
-    // try {
-    //     const {id} = req.params
-    //     const disappear = await user.findByIdAndDelete(id);
-
-    //     if (!disappear) {
-    //         return res.status(404).json({message: "User does not exist in the system."})
-    //     }
-
-    //     res.status(200).json({message: "User removed from the system!"});
-    //     console.log("Deleted!")
-
-    // } catch (error) {
-    //     res.status(500).json({message:error.message});
-    // }
-});
-
-
-
+// Error Handling
 app.use((err, req, res, next) => {
 res.status(500).send("ERROR! Something is not working properly...");
 });
+// Error Handling^^
 
+// Server, start!
 app.listen(PORT, () => {
     console.log(`Server listening on ${PORT}.`); 
-});
+}); 
+// Server, start!^^
--- routes/user.js
+++ routes/user.js
@@ -1,45 +1,43 @@
 const express = require("express");
-const router = express.Router();
+const router = express.Router(); 
+const userModel = require("../model/user");
 
-const user = require("../model/user");
-
-router
+router 
     .route("/new")
     .get(async (req, res) => {
         try{
-            const users = await user.find({});
+            const users = await userModel.find({});
             res.status(200).json(users); 
          } catch (error) {
-             res.status(500).json({message: error.message})
+             res.status(500).json({message: error.message});
          }
     });
 
 router 
-    .route("/new")
-    .post(async (req, res) => {
+    .route("/enhanced")
+    .post(async(req, res) => {
         try {
-            const brandNew = await user.create(req.body);
+            const brandNew = await userModel.create(req.body);
             res.status(200).json(brandNew);
-            console.log("New user!");
         } catch (error) {
-            res.status(500).json({message: error.message})
-        }    
+            res.status(500).json({message: error.message});
+        }
     });
 
-router 
-    .route("/new")
-    .put(async (req, res) => {
+router
+    .route("/alter/:id")
+    .put(async(req, res) => {
         try {
             const {id} = req.params
             const update = await user.findByIdAndUpdate(id, req.body);
             
-            if (!user) {
+            if (!userModel) {
                 return res.status(404).json({message: "User does not exist in system"});
             }
             const updateUser = await user.findById(id);
             res.status(200).json(update);
            
-            console.log("System Updated!")
+            console.log("System Updated!");
     
         } catch (error) {
             res.status(500).json({message:error.message});
@@ -47,20 +45,22 @@ router
     });
 
 router 
-    .route("/new/:id")
-    .delete(async (req, res) => {
+    .route("/erase/:id")
+    .delete(async(req, res) => {
         try {
             const {id} = req.params
-            const disappear = await user.findByIdAndDelete(id);
+            const disappear = await userModel.findByIdAndDelete(id);
     
             if (!disappear) {
-                return res.status(404).json({message: "User does not exist in the system."})
+                return res.status(404).json({message: "User does not exist in the system."});
             }
     
             res.status(200).json({message: "User removed from the system!"});
-            console.log("Deleted!")
+            console.log("Deleted!");
     
         } catch (error) {
             res.status(500).json({message:error.message});
         }
     });
+
+    module.exports = router;
